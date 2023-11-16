160bb16f414841e3a249cffefb46e75d121ffa9f
Bahaa-eddine-MB <bm.bouzeboudja@esi-sba.dz>
2023-11-16 23:12:49 +0100

    project config

---
+++ src/components/Global/SideBar.jsx
@@ -0,0 +1,7 @@
+export default function SideBar() {
+  return (
+  <nav>
+    
+  </nav>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary:"#4A3AFF",
+        mainRed:"#ED3159",
+        mainGray:"#8B929D",
+        mainYellow:"#ED9731",
+        mainGreen:"#55DD4A",
+        mainGray2:"#7C8DB5",
+        mainGray7:"#E6EDFF"
       },
     },
   },
   plugins: [],
-}
+};
