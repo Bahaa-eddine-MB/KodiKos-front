88743b33ad1b7978d62d62a333925ece585fac5b
Rafikkhaldi <mr.khaldi@esi-sba.dz>
2023-11-18 05:57:04 +0100

    employeeslinked

--- src/app/(dashboard)/employees/page.js
+++ src/app/(dashboard)/employees/page.js
@@ -1,25 +1,29 @@
+"use client"
 import ProjectsHeader from "@/components/ProjectsPage/ProjectsHeader";
-import ProjectCard from "@/components/ProjectsPage/ProjectCard";
 import Search from "@/components/Global/Search";
 import EmployeCard from "@/components/Employees/employeeCard";
+import { useEffect, useState } from "react";
+const axios = require('axios').default;
 
 export default function EmployeesForManager() {
+  const [employees, setEmployees] = useState([])
+
+  useEffect(()=>{
+    const getEmployees = async () => {
+      const res = await axios.post('http://localhost:4000/api/user/byRole',{
+        role: "employee"
+      })
+      setEmployees([...res.data])
+    }
+    getEmployees()
+  }, [])
+
   return (
     <div className="mt-16 ml-7 mr-10 w-full ">
       <ProjectsHeader title={"Employees"} text={"Here are your employees"}/>
       <Search />
       <div className="grid grid-cols-myGrid pb-8  gap-6">
-        <EmployeCard />
-        <EmployeCard />
-        <EmployeCard />
-        <EmployeCard />
-        <EmployeCard />
-        <EmployeCard />
-        <EmployeCard />
-        <EmployeCard />
-        <EmployeCard />
-
-        <EmployeCard />
+        {employees.map((e) => <EmployeCard key={e._id} name={e.name} role={e.role} email={e.email}/>)}
       </div>
     </div>
   );
