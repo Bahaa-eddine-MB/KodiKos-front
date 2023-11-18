1518aed3b302154aab3ce746fd10adee7d5b8dcb
Rafikkhaldi <mr.khaldi@esi-sba.dz>
2023-11-18 06:15:56 +0100

    projectslinked

--- src/app/(dashboard)/projects/page.js
+++ src/app/(dashboard)/projects/page.js
@@ -1,8 +1,20 @@
+"use client"
 import ProjectsHeader from "@/components/ProjectsPage/ProjectsHeader"
 import ProjectCard from "@/components/ProjectsPage/ProjectCard"
+import { useEffect, useState } from "react";
+const axios = require('axios').default;
 
 
 export default function ProjectsPage() {
+  const [projects, setProjects] = useState([])
+
+  useEffect(()=>{
+    const getProjects = async () => {
+      const res = await axios.get('http://localhost:4000/api/project/')
+      setProjects([...res.data])
+    }
+    getProjects()
+  }, [])
     return (
       <>
       <div className="mt-16 ml-7 mr-10 w-full">
@@ -10,17 +22,11 @@ export default function ProjectsPage() {
       <section className="w-full pt-5 pl-9 pb-16 rounded-[20px] border border-mainGray7 ">
         <h1 className="mb-7 font-medium text-2xl">Your Projects</h1>
         <div className="flex gap-10">
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+        {projects.map((p) => <ProjectCard key={p._id} title={p.title} description={p.description}/>)}
         </div>
 
       </section>
       </div>
-        
-
-
       </>
     )
 
--- src/components/ProjectsPage/ProjectCard.jsx
+++ src/components/ProjectsPage/ProjectCard.jsx
@@ -1,16 +1,16 @@
 import lamp from '../../../public/assets/ProjectsPage/ProjectCard/lamp.svg'
 import Image from 'next/image'
 
-export default function ProjectCard() {
+export default function ProjectCard({title, description}) {
     return(
         <div className='bg-mainGra10 w-fit  rounded-[20px] px-4 pt-4 pb-7 transition-all duration-700 group hover:scale-105 hover:cursor-pointer' >
             <div className='flex gap-2 '>
                 <Image src={lamp} width={25} height={30} alt=''/>
-                <h2 className='text-primary text-lg font-semibold'>Project 1</h2>
+                <h2 className='text-primary text-lg font-semibold'>{title}</h2>
             </div>
             <div className='mt-5 text-black opacity-50'>
-                <p className='max-w-[220px]'>Sigh view am high neat half to what. Sent late held than set why wife our Sigh view am high neat</p>
-                <p className='mt-5'>October 20,2020</p>
+                <p className='max-w-[220px]'>{description}</p>
+                <p className='mt-5'>November 18,2023</p>
             </div>
         </div>
     )
